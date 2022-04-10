8f3c3409950fa55e5de4aa688be23be040afde6b
JackyLin1 <jlin9702@gmail.com>
2022-04-11 06:37:36 +0900

    Update: Able to Read Message

--- src/Bot.js
+++ src/Bot.js
@@ -5,10 +5,35 @@ const { Client, Intents } = require('discord.js');
 
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [
+	Intents.FLAGS.GUILDS,
+	Intents.FLAGS.GUILD_MESSAGES
+]});
+
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
+client.on('ready', () => {
 	console.log('Ready!');
 });
+
+client.on ('messageCreate', (message) => {
+	console.log(message.content);
+})
+
 client.login(process.env.DISCORDJS_BOT_TOKEN);
+
+
+
+	// if (isReady && message.content === 'HI') {
+	// 	isReady = false;
+	// 	var voiceChannel = message.member.voiceChannel;
+	// 	voiceChannel.join()
+	// 	// .then(connection => {
+	// 	// 	const dispatcher = connection.playFile ('./Audio/gab.mp3');
+	// 	// 	dispatcher.on ("end", end => {
+	// 	// 		voiceChannel.leave();
+	// 	// 	});	
+	// 	// })
+	// 	// .catch(err => console.log(err));
+	// 	// isReady = true;
+	// }
